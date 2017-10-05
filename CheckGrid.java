4584a23ac8243bfdb2d17c1a04cf4c9f8fb3bf93
Nick Erbach <erbac100@mail.chapman.edu>
2017-10-05 08:51:50 -0700

    Add files via upload

--- CheckGrid.java
+++ CheckGrid.java
@@ -1,12 +1,17 @@
-//package com.SudokuChecker;
-
 import java.io.*;
 import java.util.*;
 
 public class CheckGrid {
 
     private static int grid[][] = new int[9][9];
-    private static int missingNumber;
+    private static int missingRowNumber = -1;
+    private static int missingColNumber = -1;
+    private static int missingSubGridNumber = -1;
+    private static int errorCount;
+    private static String rowMessage = " ";
+    private static String columnMessage = " ";
+    private static String subGridMessage = " ";
+
     private static void readInGrid(String file) {
 
         try {
@@ -51,19 +56,33 @@ public class CheckGrid {
             for (int i = 0; i < 9; ++i) {
                 for (int j = 0; j < 9; ++j) {
 
-                    if(!columnSet.add(grid[j][i])) {
+                    if(!columnSet.add(grid[i][j])) {
 
                         columnError = i;
                         System.out.println("Column thread found failure in column " + (i+1));
-                        i = 9;j = 9;
-                        columnSet.clear();
+                        Integer[] tempArray = columnSet.toArray(new Integer[columnSet.size()]);
+                        Arrays.sort(tempArray);
+                        for(int x = 1;x<10;++x)
+                        {
+                          if(Arrays.asList(tempArray).contains(x)== false)
+                          {
+                            missingColNumber = x;
+                            System.out.println("Made it.");
+                            System.out.println("**Error in column " + (i+1) + ", there was a duplicate " + grid[i][j] + " in the column."+ "\n" + "You can fix this error by replacing the duplicate "+grid[i][j]+" with a "+ missingColNumber);
+                          }
+                        }
+
+                        //i = 9;j = 9;
+                        //columnSet.clear();
 
                     }
 
                     if (j == 8) {
+
                         columnSet.clear();
                     }
                 }
+
             }
         }
 
@@ -92,10 +111,17 @@ public class CheckGrid {
                         {
                           if(Arrays.asList(tempArray).contains(x)== false)
                           {
-                            missingNumber = x;
+                            missingRowNumber = x;
+                            System.out.println("**Error in row " + (i+1) + ", there was a duplicate " + grid[i][j] + " in the row."+ "\n" + "You can fix this error by replacing the duplicate "+grid[i][j]+" with a "+ missingRowNumber);
                           }
+                          //System.out.println(missingRowNumber);
                         }
-                        i = 9;j = 9;
+                        /*if(missingRowNumber != -1)
+                        {
+                          rowMessage = "Error in row " + (i+1) + ", there was a duplicate " + grid[i][j] + " in the row."+ "\n" + "You can fix this error by replacing the duplicate "+grid[i][j]+" with a "+ missingRowNumber;
+
+                        }*/
+                        //i = 9;j = 9;
                         rowSet.clear();
 
                     }
@@ -124,75 +150,26 @@ public class CheckGrid {
 
                 if (!checkSet.add(grid[i][j]))  {
                     System.out.println("Thread checking subgrid " + subGridNum + " found an error at row: " + (i+1) + ", column: " + (j+1));
+                    Integer[] tempArray = checkSet.toArray(new Integer[checkSet.size()]);
+                    //Arrays.sort(tempArray);
+                    for(int x = 1;x<10;++x)
+                    {
+                      if(Arrays.asList(tempArray).contains(x)== false)
+                      {
+                        missingSubGridNumber = x;
+                        System.out.println("**Error in sub grid " + subGridNum + ", there was a duplicate " + grid[i][j] + " in the sub grid."+ "\n" + "You can fix this error by replacing the duplicate "+grid[i][j]+" with a "+ missingSubGridNumber);
+                      }
+                    }
+                    /*if(missingSubGridNumber != -1)
+                    {
+                      subGridMessage = "Error in sub grid " + subGridNum + ", there was a duplicate " + grid[i][j] + " in the sub grid."+ "\n" + "You can fix this error by replacing the duplicate "+grid[i][j]+" with a "+ missingSubGridNumber;
+
+                    }*/
                     i = rowStop; j = columnStop;
                     checkSet.clear();
                 }
             }
         }
-    }
-
-    static private class CheckUpperLeftGrid extends Thread {
-
-        public void run() {
-            subGridCheck(0,3,0,3,1);
-        }
-
-    }
-
-    static private class CheckUpperMiddleGrid extends Thread {
-
-        public void run() {
-            subGridCheck(0,3,3,6,2);
-        }
-
-    }
-
-    static private class CheckUpperRightGrid extends Thread {
-
-        public void run() {
-            subGridCheck(0,3,6,9,3);
-        }
-
-    }
-
-    static private class CheckMiddleLeftGrid extends Thread {
-
-        public void run() {
-            subGridCheck(3,6,0,3,4);
-        }
-
-    }
-
-    static private class CheckMiddleGrid extends Thread {
-
-        public void run() {
-            subGridCheck(3,6,3,6,5);
-        }
-
-    }
-
-    static private class CheckMiddleRightGrid extends Thread {
-
-        public void run() {
-            subGridCheck(3,6,6,9,6);
-        }
-
-    }
-
-    static private class CheckBottomLeftGrid extends Thread {
-
-        public void run() {
-            subGridCheck(6,9,0,3,7);
-        }
-
-    }
-
-    static private class CheckBottomMiddleGrid extends Thread {
-
-        public void run() {
-            subGridCheck(6,9,3,6,8);
-        }
-
     }
 
     static private class CheckSubGrids extends Thread {
@@ -213,33 +190,17 @@ public class CheckGrid {
 
     public static void main(String args[]) {
 
-        readInGrid("C:/Users/Nick/Desktop/CPSC380/Project1/test3.txt");
+        readInGrid("C:/Users/Nick/Desktop/CPSC380/Project1/test4.txt");
 
         ArrayList<Thread> threads = new ArrayList<>();
 
         CheckColumns columnThread = new CheckColumns();
         CheckRows rowThread = new CheckRows();
-        /*CheckUpperLeftGrid subGrid1 = new CheckUpperLeftGrid();
-        CheckUpperMiddleGrid subGrid2 = new CheckUpperMiddleGrid();
-        CheckUpperRightGrid subGrid3 = new CheckUpperRightGrid();
-        CheckMiddleLeftGrid subGrid4 = new CheckMiddleLeftGrid();
-        CheckMiddleGrid subGrid5 = new CheckMiddleGrid();
-        CheckMiddleRightGrid subGrid6 = new CheckMiddleRightGrid();
-        CheckBottomLeftGrid subGrid7 = new CheckBottomLeftGrid();
-        //CheckBottomMiddleGrid subGrid8 = new CheckBottomMiddleGrid();*/
         CheckSubGrids subGridThread = new CheckSubGrids();
 
         threads.add(new Thread(columnThread));
-        threads.add(new Thread(rowThread));
-        threads.add(new Thread(subGridThread));
-        /*threads.add(new Thread(subGrid2));
-        threads.add(new Thread(subGrid3));
-        threads.add(new Thread(subGrid4));
-        threads.add(new Thread(subGrid5));
-        threads.add(new Thread(subGrid6));
-        threads.add(new Thread(subGrid7));
-        //threads.add(new Thread(subGrid8));
-        threads.add(new Thread(subGrid9));*/
+      //  threads.add(new Thread(rowThread));
+      //  threads.add(new Thread(subGridThread));
 
         for (Thread t : threads) {
             t.start();
@@ -261,9 +222,26 @@ public class CheckGrid {
         if(cError == -1 && rError == -1) {
             System.out.println("There were no errors in the sudoku grid.");
         } else {
-            System.out.println("Threads done. The error occured at [" + (rError+1) + ',' + (cError+1) + "].");
-            System.out.println("There was a duplicate " + grid[rError][cError] + " that caused the error." );
-            System.out.println("Because of this duplicate, there is a missing " +missingNumber +" in your row, column, or subgrid, which is causing your error.");
+            if(rowMessage != " ")
+            {
+              System.out.println(rowMessage);
+            }
+            if(columnMessage != " ")
+            {
+              System.out.println(columnMessage);
+            }
+            if(subGridMessage != " ")
+            {
+              System.out.println(subGridMessage);
+            }
+            //System.out.println(rowMessage);
+            //System.out.println(columnMessage);
+            //System.out.println(subGridMessage);
+            //System.out.println("row" +missingRowNumber);
+            //System.out.println("col" +missingColNumber);
+            //System.out.println("Threads done. The error occured at [" + (rError+1) + ',' + (cError+1) + "].");
+            //System.out.println("There was a duplicate " + grid[rError][cError] + " that caused the error." );
+            //System.out.println("Because of this duplicate, there is a missing " +missingRowNumber +" in your row, column, or subgrid, which is causing your error.");
         }
     }
 }
